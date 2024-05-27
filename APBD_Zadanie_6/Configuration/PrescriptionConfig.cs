88448277546e7579c5699c46f44ecd069ead5c2a
s26225 <s26225@pjwstk.edu.pl>
2024-05-27 18:48:38 +0200

    Init

---
+++ APBD_Zadanie_6/Configuration/PrescriptionConfig.cs
@@ -0,0 +1,42 @@
+﻿using APBD_Zadanie_6.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace APBD_Zadanie_6.Configuration
+{
+    internal class PrescriptionConfig : IEntityTypeConfiguration<Prescription>
+    {
+        public void Configure(EntityTypeBuilder<Prescription> builder)
+        {
+            builder.HasKey(e => e.IdPrescription).HasName("IdPrescription_PK");
+            builder.Property(e => e.IdPrescription).UseIdentityColumn();
+
+            builder.Property(e => e.Date).IsRequired();
+            builder.Property(e => e.DueDate).IsRequired();
+
+            builder.HasOne(e => e.IdPatientNav)
+                .WithMany(e => e.Prescriptions)
+                .HasForeignKey(e => e.IdPatient)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Patient_Prescription_FK");
+
+
+            builder.HasOne(e => e.IdDoctorNav)
+                .WithMany(e => e.Prescriptions)
+                .HasForeignKey(e => e.IdDoctor)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Doctor_Prescription_FK");
+
+            var prescriptions = new List<Prescription>();
+
+            prescriptions.Add(new Prescription
+            {
+                IdPrescription = 0,
+                Date = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(5),
+                IdPatient = 0,
+                IdDoctor = 0
+            });
+        }
+    }
+}
---
+++ APBD_Zadanie_6/Models/Prescription.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_Zadanie_6.Models
+{
+    public class Prescription
+    {
+        public int IdPrescription { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public int IdPatient { get; set; }
+        public int IdDoctor { get; set; }
+        public virtual Patient IdPatientNav { get; set; }
+        public virtual Doctor IdDoctorNav { get; set; }
+        public virtual ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; } = new List<PrescriptionMedicament>();
+    }
+}
